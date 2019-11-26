f34f2c15b8415b4c539f28672d752b73385d40de
jabernal89 <58176416+jabernal89@users.noreply.github.com>
2019-11-26 15:17:08 -0300

    cambio simple

--- src/test/java/edu/grupoesfera/asignador/AsignadorDeVehiculoDeEntregaTest.java
+++ src/test/java/edu/grupoesfera/asignador/AsignadorDeVehiculoDeEntregaTest.java
@@ -20,7 +20,6 @@ public class AsignadorDeVehiculoDeEntregaTest {
      AsignadorDeVehiculo asignador = new AsignadorDeVehiculo();
      Envio envio = asignador.Asignar(paquetes);
      assertThat(envio.getVehiculo()).isEqualTo("BICICLETA");
-     
 	}
 	
 	@Test
